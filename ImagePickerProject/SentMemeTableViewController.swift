07fe6f9fb8c3c27eaaeb6edbdd2171e7b5222b31
Cathy Oun <cathy810218@gmail.com>
2016-03-17 05:25:18 -0700

    TableView problem fixed

---
+++ ImagePickerProject/SentMemeTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SentMemeTableViewController.swift
+//  MakeMeme
+//
+//  Created by Cathy Oun on 3/16/16.
+//  Copyright © 2016 Cathy Oun. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+  @IBOutlet weak var tableView: UITableView!
+  var memes: [Meme] {
+    return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tableView.dataSource = self
+    self.tableView.delegate = self
+    self.tableView.registerNib(UINib(nibName: "TableViewCell", bundle: NSBundle.mainBundle()), forCellReuseIdentifier: "TableViewCell")
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    self.tableView.reloadData()
+    print("Numbers of Memes: \(self.memes.count)")
+
+  }
+  
+  //MARK: UITableViewDelegate and UITableViewDataSource
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.memes.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = self.tableView.dequeueReusableCellWithIdentifier("TableViewCell", forIndexPath: indexPath) as! TableViewCell
+    cell.cellImage.image = memes[indexPath.row].memedImage
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    //    TODO: display detail view controller
+    
+  }
+  
+}
